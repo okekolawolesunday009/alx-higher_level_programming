d26c255cd7ffb9b9f7138cdf4e99b052d46d225d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-07 14:25:08 +0100

    taskup to task8

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str= "Hoberton School"
+print(str, end="")
+print(str, end ="")
+print(str)
+print(str[:9],"\n")
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpretedd, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"5
+print(str[40:-63],str[:7],str[108:-18])
