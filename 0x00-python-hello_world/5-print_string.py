e712819bda3107cd827c6bc03d8ca5fbf3fb67ad
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-07 19:01:10 +0100

    changes

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str= "Hoberton School"
-print(str, end="")
-print(str, end ="")
-print(str)
+print(3*str)
 print(str[:9],"\n")
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpretedd, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"5
+str = "Python is an interpretedd, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
 print(str[40:-63],str[:7],str[108:-18])
