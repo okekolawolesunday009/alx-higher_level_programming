fa8fe4812c5c96a62d273dabc634b76cd4b6c6d7
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-08 16:21:45 -0700

    aph2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z')):
+for char in range(ord('a'), ord('{')):
     if char != 113 and char != 101:
-        print(chr(char), end="")
+        print("{}".format(chr(char)), end="")
