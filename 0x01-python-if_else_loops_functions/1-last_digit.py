5fd04711a4738709b7a488992720403563844a08
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-08 16:41:14 -0700

    lstdigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l = number % 10
+l = abs(number) % 10
 string = "Last digit of"
 if l > 5:
     print("{} {} is {} and is greater than 5".format(string, number, l))
 elif l == 0:
     print("{} {} is {} and is 0".format(string, number, l))
 else:
-    print("{} {} is {} and is less than 6 and not 0".format(string, number, l))
+    print("{} {} is {:d} and is less than 6 and not 0".format(string, number, l))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for tens in range(10):
-    for units in range(10):
-        if tens != 8 and units != 9:
-            print("{:01}{:01}".format(tens, units), end=", ")
-        else:
-            print("{:01}{:01}".format(tens, units), end=" ")
-       
-     
+for number in range(100):
+    tens = number // 10
+    units = number % 10
+    if tens != 8 and units != 9:
+        print("{:01}{:01}".format(tens, units), end=", ")
+    else:
+        print("{:01}{:01}".format(tens, units), end="") 
