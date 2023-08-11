26f5a0e99391815682f1871bfc760e9e5c727a97
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-10 19:13:42 -0700

    hexa

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 63):
-    if i in [10, 11, 12, 13, 14, 15]:
+for i in range(0, 99):
        print("{} = 0X{}".format(i,hex(i)[2:]))
-    else:
-        print("{} = 0X{}".format(i,i))
