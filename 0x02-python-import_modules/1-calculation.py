03a7bd3e2bda8897d2d46b3695a2d5a4329c2395
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-14 14:41:24 -0700

    calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,3 +7,7 @@ def calculator():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+
+if __name__ == '__main__':
+    calculator()
