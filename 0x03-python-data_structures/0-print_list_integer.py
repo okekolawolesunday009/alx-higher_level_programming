4a1b5a33d21f73f88f0227f50646c08a7dbf77d6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-15 14:01:52 +0100

    ls

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
      """prints all intergers of a list"""
-     for i in range(my_list):
-        print("{:d}".format(i))
+     for i in range(my_list=[]):
+        print("{}".format(my_list[i]))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-         my_list[idx] = element     
+     """ replaces an element in a list at a specific position without modifying the original list"""
+     if idx >= 0 and idx < len(my_list):
+         my_list[idx] = element
          return my_list
-    else:
+     else:
           return None
