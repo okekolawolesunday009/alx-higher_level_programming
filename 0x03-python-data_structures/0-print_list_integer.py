7d7db6f959cdb85e4be1b3f6baa9e89cc5b4effe
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-15 09:49:10 -0700

    main

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,3 +3,7 @@ def print_list_integer(my_list=[]):
     """prints intergers of a list"""
     for i in my_list:
         print("{}".format(i))
+
+
+if __name__ == "__main__":
+    print_list_integer()
