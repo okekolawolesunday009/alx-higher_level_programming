5e1a7986b951fbf543c81dba2ec6bf73cb6a3af5
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-15 10:10:22 -0700

    all

---
+++ 0x03-python-data_structures/10-divisble_by_2.py
@@ -0,0 +1,6 @@
+
+def divisible_by_2(my_list=[]):
+    result_list = []
+    for i in my_list:
+        result_list.append(i % 2 == 0)
+    return result_list
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    tuple = (len(sentence), sentence[0] if len(sentence) > 0 else None)
+    return tuple
