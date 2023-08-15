503a3b8ad721e9c55d469889d1c7d9cdbbb58d2a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-15 09:55:16 -0700

    a

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def print_list_integer(my_list, idx):
+def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
-         return my_list[idx]
+        return my_list[idx]
     else:
         return None
+
+
+if __name__ == "__main__":
+    element_at()
