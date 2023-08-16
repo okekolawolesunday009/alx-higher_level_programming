b9642899d82d0f17a1c9945a10f91acc620d9a3d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-15 19:08:22 -0700

    no_c

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-     """ replaces an element in a list at a specific position without modifying the original list"""
-     if idx >= 0 and idx < len(my_list):
-         new_list = my_list[:]
-         new_list[idx] = element
-         return new_list
-     else:
-          return None
+    if idx >= 0 and idx < len(my_list):
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+    else:
+        return None
 
 
 if __name__ == "__main__":
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-  """removes all'c' and 'C' from a string"""
-  for i in range(0, len(my_string)):
-      if i != 'c'  and i != 'C':
-          print('{}'.format(my_string[i]))
+    """removes all'c' and 'C' from a string"""
+    for i in range(0, len(my_string)):
+        if i != 'c'  and i != 'C':
+            print('{}'.format(my_string[i]))
+
+
+if __name__ == "__main__":
+    no_c()
