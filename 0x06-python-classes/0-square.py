e0bad264864dd9ef6d849847a37331e72f6e80db
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-29 13:38:13 -0700

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Module: 0-square
-This module defines a basic Square class.
-
-The Square class is designed to represent a simple square shape. It provides a
-foundation for more advanced functionalities that can be built upon it.
-
-Example:
-    Create an instance of the Square an empty class and print its details.
-"""
-
-
+"""Define a class Square."""
 
 class Square:
     """Represents a square.
