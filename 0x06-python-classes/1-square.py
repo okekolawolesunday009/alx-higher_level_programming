a2bd43c49a75cec4948ca7eeb2cc54668e4b6b5d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-29 21:43:39 +0100

    02

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 class Square:
-     def __init__(self, size):
-         self.__size = size
+    """Represents a square.
+
+    This class defines a basic square. More details about the class and its
+    usage can be provided here.
+
+    Attributes:
+        size
+
+    Methods:
+        Nil
+
+    Example:
+        <class '0-square.Square'>
+        {}
+    """
+    def __init__(self, size):
+        """
+         Args:
+            self: represents the instance of class.
+            size (int) : the size of shape passed.
+         Returns:
+            int: size of int otherwise valueEror
+        """
+        self.__size = size
