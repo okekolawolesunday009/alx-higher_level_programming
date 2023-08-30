3fb42dc570357c8998970466741703c6a28aadec
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-08-30 17:44:29 +0100

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,10 +2,33 @@
 """Define a class Square."""
 
 class Square:
-     def __init__(self, size=0):
+    """Represents a square.
+
+    This class defines a basic square. More details about the class and its
+    usage can be provided here.
+
+    Attributes:
+        size
+
+    Methods:
+        Nil
+
+    Example:
+        <class '2-square.Square'>
+        {'_Square__size': 3}
+        <class '2-square.Square'>
+        {'_Square__size': 0}
+        'Square' object has no attribute 'size'
+        'Square' object has no attribute '__size'
+        size must be an integer
+        size must be >= 0
+    """
+    def __init__(self, size=0):
         """Initializes a new square
-           Args:
-            size (int): The size of the new square.
+            Args:
+                size (int): The size of the new square.
+            Return:
+                size
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
