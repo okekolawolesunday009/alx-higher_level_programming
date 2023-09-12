e2241020e328e9c53afeaf915ffb1e8e0b8c2f14
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-12 09:54:28 -0700

    json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 
 
 import json
+
 def from_json_string(my_str):
     """functionize of JSON to PY DATA STRUCTURE"""
     py_string = json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import json
+""""Defines to write obj file to JSON"""
 
+
+import json
 
 def save_to_json_file(my_obj, filename):
     with open(filename, mode='w', encoding='utf-8') as file:
