ff87d28d69d4a509fe68e13304a4c49967dbe1a3
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-12 21:54:24 +0100

    py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+"""script that adds all arguments to a Python list
+    and then saves to a file
+"""
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
 filename = "add_item.json"
 
 if __name__ == "__main__":
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,12 @@
 #!/bin/usr/python3
+"""function that returns the dictionary description 
+    with simple data structure
+"""
+
 
 def class_to_json(obj):
+    """function returns dict
+        Args:
+            obj(instance of a class): element which dict is to be printed out
+    """
     return obj.__dict__
