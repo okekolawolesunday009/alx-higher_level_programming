25c3e1a3216231021db1270da89b34e47675926e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-12 07:58:40 -0700

    py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import json
+"""PY data from JSON"""
+
 
 def from_json_string(my_str):
+    """functionize of JSON to PY DATA STRUCTURE"""
     py_string = json.loads(my_str)
     return (py_string)
-    print(py_string)
