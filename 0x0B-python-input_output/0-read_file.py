781ca90d95e5b5f71f7af9f6e963b078449d6cb9
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-12 13:14:01 +0100

    i/o

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Reads a file from main"""
+
+
+def read_file(filename=""):
+    """reads a file out to the console"""
+    with open(filename, "r", encoding='utf-8') as file:
+        data = file.read()
+    print(data)
