2d674b62dd069af33cd456ad1f1651656d61602a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-12 09:48:16 -0700

    live

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""append string"""
 
 
 def append_write(filename="", text=""):
+    """append the string to file"""
     try:
         with open(filename, "a", encoding='utf-8') as file:
             lines = file.write(text)
