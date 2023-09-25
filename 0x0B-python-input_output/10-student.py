2c94b304b54e66ec5aaf642fd686556cf07ccb0f
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-25 04:00:36 -0700

    10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,46 @@
 #!/usr/bin/python3
+""" Class that defines a student """
+
 
 class Student:
+    """
+    Class that defines a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if (attrs == type(str)):
-            return (getattr(self, attrs) for attr in attrs)
-        if (attrs == type(str)):
-            return self.__dict__ 
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict containig the attributes of the student instance
+            attrs: optional
+        """
+        if attrs is None:
+            return {
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'age': self.age
+                    }
+        else:
+            json_data = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_data[attr] = getattr(self, attr)
+            return json_data
