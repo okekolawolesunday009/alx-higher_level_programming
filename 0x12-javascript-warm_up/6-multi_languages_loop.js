8ebc911de0aa2a71687c5f654455198a0e5c7829
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-13 17:58:05 -0800

    code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrays = ['C is fun', 'Python is cool', 'JavaScipt is amzing'];
+const arrays = ['C is fun', 'Python is cool', 'JavaScipt is amazing'];
 
 for (let i = 0; i < arrays.length; i++) {
   console.log(arrays[i]);
