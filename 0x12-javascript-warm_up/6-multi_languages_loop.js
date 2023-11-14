b21acce7033e8aaa9d68f9f2527e0f0d7a25f6dc
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-13 18:02:16 -0800

    code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrays = ['C is fun', 'Python is cool', 'JavaScipt is amazing'];
+const arrays = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arrays.length; i++) {
   console.log(arrays[i]);
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const  argv  = process.argv.slice(2);
+const argv  = process.argv.slice(2);
 
-const arg_1 = parseInt(argv[0]);
-const arg_2 = parseInt(argv[1]);
+const argOne = parseInt(argv[0]);
+const argTwo = parseInt(argv[1]);
 
 function add (a, b) {
      return console.log(a + b);
 }
-add(arg_1, arg_2);
+add(argOne, argTwo);
