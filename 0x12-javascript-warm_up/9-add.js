1c9edf381805b9673a58f8470113dcdb462c249a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-13 18:06:34 -0800

    code

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argv  = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
 const argOne = parseInt(argv[0]);
 const argTwo = parseInt(argv[1]);
 
 function add (a, b) {
-     return console.log(a + b);
+	return console.log(a + b);
 }
 add(argOne, argTwo);
