720ac6ff27ba571e91fc54f5cb904f581ea8c293
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-14 17:49:38 -0800

    betty

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
-module.exports  = class Rectangle {
-    constructor(h, w) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        this.width = undefined;
-        this.height = undefined;
-      } else {
-        this.height = h;
-        this.width = w;
+module.exports = class Rectangle {
+  constructor (h, w) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.height = h;
+      this.width = w;
+    }
+  }
 
-      }
-     
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      console.log('X'.repeat(this.height));
     }
-    print() {
-      for (let i = 0; i < this.width; i++) {
-        console.log('X'.repeat(this.height));
-      }
-    }
-}
+  }
+};
