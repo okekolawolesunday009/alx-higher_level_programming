e7cf89c7b3ef2dce5639ea8b8954e77047331fb9
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-14 18:27:17 -0800

    code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    for (let i = list.length; i < 0; i--) {
-        console.log(list[i])
-    }
-}
+  const reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
+  return (reversed);
+};
