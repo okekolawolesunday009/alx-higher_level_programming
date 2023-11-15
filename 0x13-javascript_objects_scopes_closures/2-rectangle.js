ca61606caf931e665970948b43a3379025139558
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-14 17:41:30 -0800

    code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
-module.exports  = class Rectangle {
-    constructor(h, w) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        this.height = undefined;
-        this.width = undefined;
-      } else {
-        this.height = h;
-        this.width = w;
-
-      }
-    }
-  }
-
+module.exports = class Rectangle {
+	constructor (h, w) {
+		if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+			this.height = undefined;
+			this.width = undefined;
+		} else {
+			this.height = h;
+			this.width = w;
+		}
+	}
+}
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,17 +17,14 @@ module.exports = class Rectangle {
             console.log('X'.repeat(this.height));
           }
     }
+	 double() {
+	   	 this.width *= 2;
+    		this.height *= 2;
+	 }
 
-    rotate() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-          }
-
-    }
-    double() {
-        for (let i = 0; i < this.width * 2; i++) {
-            console.log('X'.repeat(this.heigth * 2));
-          }
-
-    }
+  rotate () {
+    const tmp = this.width;
+    	this.width = this.height;
+   	 this.height = tmp;
+}
 }
