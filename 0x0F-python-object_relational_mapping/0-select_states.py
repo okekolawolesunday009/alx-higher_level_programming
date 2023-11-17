9b1b5dbe32e3e3f13bf85ee6189f225a8e87ea85
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-16 18:00:56 -0800

    sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-
+import MySQLdb
+from sys import argv, exit
 
 if __name__ == "__main__":
-    import MYSQLdb
-    from sys import argv, exit
 
     if len(argv) != 4:
         print("Usage: {:s} <username> <password> <database>".format(argv[0]))
@@ -13,7 +11,7 @@ if __name__ == "__main__":
     pwd = argv[2]
     db = argv[3]
     #Trying to connect 
-    db_connection = MySQLdb.connect(usr=usr, passwd="", db ="hbtn_0e_0_usa",port=3306)
+    db_connection = MySQLdb.connect(host="localhost", user=usr, password=pwd, database=db,port=3306)
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
