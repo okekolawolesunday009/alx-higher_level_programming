4fc05570d522cee05b05c6fd41d32edfbf7e5530
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-19 23:23:25 +0100

    stach1

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                            format(argv[1], argv[2], argv[3],
                                   pool_pre_ping=True))
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bi:wnd=engine)
     session = Session()
 
     query = text("""
