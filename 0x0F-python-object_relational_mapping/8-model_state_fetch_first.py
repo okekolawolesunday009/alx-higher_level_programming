e98077828111a456132ca60778fe7e1b51c8727a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-19 12:03:10 -0800

    code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     password = '{}'.format(argv[2])
     db_name = '{}'.format(argv[3])
   
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(username, password, db_name,
                                   pool_pre_ping=True))
 
