10e5c03a0e6e7ed424de424b05c637f4f6070af6
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-01 18:48:43 +0000

    task-4-refix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""prints the content body respnse of url"""
 import requests
+
 
 if __name__ == "__main__":
         url = ' https://alx-intranet.hbtn.io/status'
         req = requests.get(url)
         content = req.text
         print("Body response:")
-        print("    - type:", type(content))
-        print("    - content:", (content))
+        print("\t- type:", type(content))
+        print("\t- content:", (content))
