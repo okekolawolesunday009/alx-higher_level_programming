b9ef0a4e05c71650912eebee7909756e25dc92fa
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-01 19:00:25 +0000

    fix-task-5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,9 +4,9 @@ import requests
 
 
 if __name__ == "__main__":
-        url = ' https://alx-intranet.hbtn.io/status'
-        req = requests.get(url)
-        content = req.text
-        print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", (content))
+    url = ' https://alx-intranet.hbtn.io/status'
+    req = requests.get(url)
+    content = req.text
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", (content))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""prints the X-Request-Id of the url"""
 from sys import argv, exit
-import Requests
+import requests
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
     if len(argv) != 2:
-        exit 1
+        exit(1)
     url = argv[1]
     req = requests.get(url)
-    if re1.status_code == 200:
+    if req.status_code == 200:
         ri = req.headers.get('X-Request-Id')
         if ri:
             print(ri)
@@ -15,5 +17,3 @@ if __name__ = "__main__":
             print("x-Request-Id not found")
     else:
         print(f"Error:{req.status_code}")
-
-
