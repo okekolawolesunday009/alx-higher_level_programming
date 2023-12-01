6625826de35025d34626bf36513fa1f61eadc875
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-01 18:45:03 +0000

    task1-refix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     if len(argv) != 2:
         exit(1)
     url = argv[1]
-    try:
-        with urlopen(url) as response:
-            content = response.read()
-            utf8_content = content.decode('utf-8')
-            request_id = response.getheader('X-Request-Id')
-        print(request_id)
-    except HTTPError as e:
-        print(e.reason)
+    with urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        request_id = response.getheader('X-Request-Id')
+    print(request_id)
