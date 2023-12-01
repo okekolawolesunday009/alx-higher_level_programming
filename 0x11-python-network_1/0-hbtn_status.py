3a5048f5c2936a65ab47b290fefcd316d43877fa
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-01 18:36:16 +0000

    adddoc

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"Fecthes data from the url"
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
@@ -6,10 +7,9 @@ from urllib.error import URLError, HTTPError
 if __name__ == "__main__":
     url = ' https://alx-intranet.hbtn.io/status'
     req = Request(url)
-    try:
-        with urlopen(req) as response:
-            content = response.read()
-            utf8_content = content.decode('utf-8')
+    with urlopen(req) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
         print("Body response:")
         print("    - type:", type(content))
         print("    - content:", repr(content))
