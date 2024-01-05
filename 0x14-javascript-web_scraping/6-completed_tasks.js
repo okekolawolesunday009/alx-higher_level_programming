9b0016bf0441ce44ad011bb40ef42e320aedbce3
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-01-05 01:49:44 +0000

    task 6 finall refix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,10 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
   try {
     const dataArray = JSON.parse(body);
     const userCounts = {};
