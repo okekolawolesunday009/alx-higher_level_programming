74ccad77e873ecfd4c333a315ea9beee38a0000a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-01-05 01:44:35 +0000

    fix task 4 and 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,9 @@ const url = 'https://swapi-api.alx-tools.com/api/films/';
 const filmId = process.argv[2];
 
 request.get(url + filmId, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
   try {
     const filmData = JSON.parse(body);
     console.log(filmData && filmData.title);
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,17 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function (err, res, body) {
-
-	if (err) {
-		console.log(err);
-	}
-	try {
-		const data = JSON.parse(body).results;
-		console.log(data.reduce((count, movie) => {
-			return movie.characters.find(cast => cast.endsWith('/18/')) 
-				? count + 1 : count
-		}, 0));
-	} catch (err) {
-		console.log(err);
-	}
-
+  if (err) {
+    console.log(err);
+  }
+  try {
+    const data = JSON.parse(body).results;
+    console.log(data.reduce((count, movie) => {
+      return movie.characters.find(cast => cast.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  } catch (err) {
+    console.log(err);
+  }
 });
