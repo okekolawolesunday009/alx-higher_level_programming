b028313f4d6fadbf2fb019aa729dafc184c18683
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-03-30 02:18:21 +0000

    concat

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 str1 = "Hoberton"
 str2 = "School"
-print("Welcome to {} {}!".format(str1, str2))
+str3 = f"Welcome to {str1} {str2}!"
+"""print("Welcome to {} {}!".format(str1, str2))"""
+print(str3)
